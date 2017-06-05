c3f68c3c7f3f0c086a07472b6af0ab093e258e63
Lauren?iu Andronache <laurentiu.andronache@trailung.ro>
2017-06-05 09:58:17 +0300

    This actually commits the working code for Rosie: https://github.com/Laurentiu-Andronache/byteball-chatbot-Rosie
    But it's a good template to start from, and I'll use it later to finish Mitsuku, which we all love!

--- start.js
+++ start.js
@@ -1,32 +1,32 @@
-// import { version } from './package.json';
+import { version } from './package.json';
+import * as chatbotOptions from './chatbot.json';
 
 require('byteballcore/conf.js');
 require('byteballcore/wallet.js');
-const headlessWallet = require('headless-byteball');
+require('headless-byteball');
 const eventBus = require('byteballcore/event_bus.js');
 const device = require('byteballcore/device.js');
 
-headlessWallet.setupChatEventHandlers();
-
-const chatbotPrivateKeys = require('./chatbotPrivateKeys');
 const Pandorabot = require('pb-node');
 
-const bot = new Pandorabot(chatbotPrivateKeys);
+const bot = new Pandorabot(chatbotOptions);
 const Sessions = {};
+
 
 function sendGreeting(deviceAddress) {
   device.sendMessageToDevice(deviceAddress, 'text',
-`Created by @la from byteball.slack.com
+      `Created by @la from byteball.slack.com
 Open source: https://github.com/Laurentiu-Andronache/byteball-chatbot-Mitsuku
-Version: 0.0.1
------------------------------------------------------------------------------`);
+Version: ${version}
+-------------------------------------------------------------`);
 }
-// ${version}
+
 
 function initiateSession(deviceAddress) {
   Sessions[deviceAddress] = {};
-  Sessions[deviceAddress].InitializationTimestamp = Date.now();
+  Sessions[deviceAddress].PandorabotsDevice = deviceAddress.toLowerCase();
 }
+
 
 eventBus.on('paired', (deviceAddress) => {
   if (!Sessions[deviceAddress]) {
@@ -42,12 +42,22 @@ eventBus.on('text', (deviceAddress, text) => {
   }
 
   const talkParams = {
-    sessionid: deviceAddress,
+    client_name: Sessions[deviceAddress].PandorabotsDevice,
     input: text };
+//   console.log(`
+// talkParams.client_name: ${talkParams.client_name}
+// talkParams.input: ${talkParams.input}`);
 
   bot.talk(talkParams, (err, res) => {
     if (!err) {
-      device.sendMessageToDevice(deviceAddress, 'text', res);
+      try {
+        // console.log(`\n${res}`);
+        device.sendMessageToDevice(deviceAddress, 'text', res.responses[0]);
+      } catch (e) {
+        console.log(`\n${e}`);
+      }
+    } else {
+      console.log(`\n${err}`);
     }
   });
 });
